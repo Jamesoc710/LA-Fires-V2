8e89824b5c55779f1b4deaaf94aaa1d682622384
Miguel <MiguelPimienta19@gmail.com>
2025-04-25 16:35:37 -0700

    Made landing page template dynamic

--- gemini-chatbot/app/landing/page.tsx
+++ gemini-chatbot/app/landing/page.tsx
@@ -28,7 +28,7 @@ export default function LandingPage() {
   return (
     <main className="min-h-screen overflow-x-hidden bg-white dark:bg-gray-900 text-black dark:text-white py-24 sm:py-32 flex items-center justify-center">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid items-stretch max-w-none grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:grid-cols-[1fr_3fr]">
           
           {/* Text column */}
           <div className="lg:pt-4 lg:pr-8">
@@ -61,16 +61,14 @@ export default function LandingPage() {
           </div>
 
           {/* Image column */}
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center h-full ">
             <video
               src="BuildingCodeAssistantDemo.mp4"
               autoPlay
               loop
               muted
               playsInline
-              width={2432}
-              height={1442}
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              className="h-full w-full object-cover rounded-xl shadow-xl ring-1 ring-gray-400/10"
             />
           </div>
 
